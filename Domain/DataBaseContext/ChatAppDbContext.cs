75fcf3b5569f4ff538b9e6a81fd2e8b979f5ab8d
Ghostyshka <74868817+Ghostyshka@users.noreply.github.com>
2024-07-06 17:17:03 +0300

    added: entity's
    chat
    message
    user

---
+++ Domain/DataBaseContext/ChatAppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.DataBaseContext;
+
+public class ChatAppDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<Chat> Chats { get; set; }
+    public DbSet<Message> Messages { get; set; }
+
+    public ChatAppDbContext(DbContextOptions<ChatAppDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>()
+            .HasMany(u => u.Chats)
+            .WithOne(c => c.Creator)
+            .HasForeignKey(c => c.CreatorId);
+
+        modelBuilder.Entity<Chat>()
+            .HasMany(c => c.Messages)
+            .WithOne(m => m.Chat)
+            .HasForeignKey(m => m.ChatId);
+    }
+}
---
+++ Domain/Models/Chat.cs
@@ -0,0 +1,10 @@
+﻿namespace Data.Models;
+
+public class Chat
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int CreatorId { get; set; }
+    public User Creator { get; set; }
+    public ICollection<Message> Messages { get; set; }
+}
