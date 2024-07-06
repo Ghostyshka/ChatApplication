475b0725e8bf681eeae053184f11840c9ad609f8
Ghostyshka <74868817+Ghostyshka@users.noreply.github.com>
2024-07-06 17:42:08 +0300

    updated: program, added: SignalR, services and Db context

--- ChapApp/Program.cs
+++ ChapApp/Program.cs
@@ -1,11 +1,25 @@
+using Application.IServices;
+using Application.Services;
+using Data.DataBaseContext;
+using Microsoft.EntityFrameworkCore;
+using Presentation.ChatHub;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add SignalR
+builder.Services.AddSignalR();
+
+// Add database context
+builder.Services.AddDbContext<ChatAppDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add application services
+builder.Services.AddScoped<IChatService, ChatService>();
 
 var app = builder.Build();
 
@@ -16,11 +30,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapHub<ChatHub>("/chatHub");
 
 app.Run();
