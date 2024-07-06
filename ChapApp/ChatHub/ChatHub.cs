d1ea5fe5ec98c418f5c83f9680e7b0a340505a6b
Ghostyshka <74868817+Ghostyshka@users.noreply.github.com>
2024-07-06 17:20:22 +0300

    added: Chat Hub

---
+++ ChapApp/ChatHub/ChatHub.cs
@@ -0,0 +1,30 @@
+﻿using Application.IServices;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Presentation.ChatHub;
+
+public class ChatHub : Hub
+{
+    private readonly IChatService _chatService;
+
+    public ChatHub(IChatService chatService)
+    {
+        _chatService = chatService;
+    }
+
+    public async Task SendMessage(int chatId, int userId, string message)
+    {
+        var msg = await _chatService.SendMessageAsync(chatId, userId, message);
+        await Clients.Group(chatId.ToString()).SendAsync("ReceiveMessage", msg);
+    }
+
+    public async Task JoinChat(int chatId)
+    {
+        await Groups.AddToGroupAsync(Context.ConnectionId, chatId.ToString());
+    }
+
+    public async Task LeaveChat(int chatId)
+    {
+        await Groups.RemoveFromGroupAsync(Context.ConnectionId, chatId.ToString());
+    }
+}
